37851d5b265476190ab8985c2fa78299804e8e06
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-11 16:46:23 +0500

    added basic layout for dark-mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
 
 function App() {
-  return <h2>dark mode starter</h2>
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn">toggle</button>
+        </div>
+      </nav>
+      <section className="articles">
+        <article className="post">
+          <h2>the WET Codbase</h2>
+          <div className="post-info">
+            <span>Sunday 4th, 2020</span>
+            <span>11 min read</span>
+          </div>
+          <p>Come waste your time with me</p>
+        </article>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
