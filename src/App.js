d0735368f54f2f01476c07343c27d82607327b6e
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-11 17:32:23 +0500

    toggle dark-mode functionality added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import data from './data';
 import Article from './Article';
 
 function App() {
-  const [toggle, setToggle] = useState(false);
+  const [theme, setTheme] = useState('light-theme');
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    document.documentElement.classList.toggle('dark-theme');
+    console.log(document.documentElement.classList);
+  };
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn">toggle</button>
+          <button className="btn" onClick={handleChange}>
+            toggle
+          </button>
         </div>
       </nav>
       <section className="articles">
